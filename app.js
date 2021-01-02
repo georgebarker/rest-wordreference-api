ff234577d83002454377821814ced03aa56c74f8
georgebarker <georgebarker1@me.com>
2021-01-02 23:26:48 +0100

    Remove dummy endpoint

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ app.listen(3000, () => {
   console.log('Server running on port 3000')
 })
 
-app.get('/url', (req, res, next) => {
-  res.json(['Tony', 'Lisa', 'Michael', 'Ginger', 'Food'])
-})
-
 app.get('/api/:word/:from/:to/', async (req, res, next) => {
   console.log(req.params)
   const result = await wr(req.params.word, req.params.from, req.params.to)
