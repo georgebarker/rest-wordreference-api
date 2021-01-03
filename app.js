47d0671d8be5d3ee60197a48bf0426f90711127d
georgebarker <georgebarker1@me.com>
2021-01-03 14:45:57 +0100

    Correct Licence type in package.json and extract port number to const

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const wr = require('wordreference-api')
 const express = require('express')
 const app = express()
+const port = 3000
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000')
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
 })
 
 app.get('/api/:word/:from/:to/', async (req, res, next) => {
